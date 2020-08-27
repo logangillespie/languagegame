11065e35d0bdb9398099d8601eb6e8426b4805eb
thuyvanv <50762956+thuyvanv@users.noreply.github.com>
2020-08-26 21:50:26 -0400

    added navigation controller

---
+++ languagegame1/languagegame1/CreationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreationViewController.swift
+//  languagegame1
+//
+//  Created by Thuyvan on 8/26/20.
+//  Copyright © 2020 Thuyvan. All rights reserved.
+//
+
+import UIKit
+
+class CreationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        IBAction
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
