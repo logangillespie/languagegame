71b9d63fe52af6c9252e23167746c9d820fb4777
thuyvanv <50762956+thuyvanv@users.noreply.github.com>
2020-10-07 23:02:44 -0400

    linked buttons together

--- languagegame1/TriviaViewController.swift
+++ languagegame1/TriviaViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 class TriviaViewController: UIViewController {
 
     @IBOutlet weak var triviaImage: UILabel!
+    @IBOutlet weak var option1: UIButton!
+    @IBOutlet weak var option2: UIButton!
+    @IBOutlet weak var option3: UIButton!
+    @IBOutlet weak var option4: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
