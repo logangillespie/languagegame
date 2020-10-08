841ddaf20ec3d25b0e5dd915c4888bb756273e00
thuyvanv <50762956+thuyvanv@users.noreply.github.com>
2020-10-07 22:48:50 -0400

    added navigation controller

---
+++ languagegame1/TriviaViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TriviaViewController.swift
+//  languagegame1
+//
+//  Created by Thuyvan on 10/7/20.
+//  Copyright © 2020 Thuyvan. All rights reserved.
+//
+
+import UIKit
+
+class TriviaViewController: UIViewController {
+
+    @IBOutlet weak var triviaImage: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
