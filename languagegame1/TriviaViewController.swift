642ddc8a5cb9bd2bfa4d06738b44878a89b78a2c
thuyvanv <50762956+thuyvanv@users.noreply.github.com>
2020-10-31 17:33:42 -0400

    font

--- languagegame1/TriviaViewController.swift
+++ languagegame1/TriviaViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+/* website used: https://www.seemuapps.com/swift-make-a-trivia-quiz-game */
+
 class TriviaViewController: UIViewController {
 
     @IBOutlet weak var triviaImage: UILabel!
@@ -15,6 +17,33 @@ class TriviaViewController: UIViewController {
     @IBOutlet weak var option2: UIButton!
     @IBOutlet weak var option3: UIButton!
     @IBOutlet weak var option4: UIButton!
+    
+    struct Question {
+          let question: String
+          let answers: [String]
+          let correctAnswer: Int
+      }
+      
+      
+      var questions: [Question] = [
+          Question(
+              question: "What is the correct term to say hello?",
+              answers: ["Hello", "Hola", "Hallo", "Ciao"],
+              correctAnswer: 2),
+         /* Question(
+              question: "Have you subscrbed to Seemu Apps",
+              answers: ["Yes", "No", "I will", "No Thanks"],
+              correctAnswer: 0),
+          Question(
+              question: "What is the Capital of Australia?",
+              answers: ["Sydney", "Melbourne", "Adelaide", "Canberra"],
+              correctAnswer: 3) */
+      ]
+      
+      var currentQuestion: Question?
+      var currentQuestionPos = 0
+      
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,13 +51,49 @@ class TriviaViewController: UIViewController {
     }
     
     @IBAction func onButton1(_ sender: Any) {
+        checkAnswer(idx:0)
     }
     @IBAction func onButton2(_ sender: Any) {
+        checkAnswer(idx:1)
     }
     @IBAction func onButton3(_ sender: Any) {
+          checkAnswer(idx:2)
     }
     @IBAction func onButton4(_ sender: Any) {
+          checkAnswer(idx:3)
     }
+    
+    // Check if an answer is correct then load the next question
+    func checkAnswer(idx: Int) {
+        if(idx == currentQuestion!.correctAnswer) {
+           // noCorrect += 1 //
+        }
+        loadNextQuestion()
+    }
+    
+    func loadNextQuestion() {
+        // Show next question
+        if(currentQuestionPos + 1 < questions.count) {
+            currentQuestionPos += 1
+            currentQuestion = questions[currentQuestionPos]
+            setQuestion()
+        // If there are no more questions show the results
+        } else {
+            performSegue(withIdentifier: "sgShowResults", sender: nil)
+        }
+        
+
+    }
+    
+    // Set labels and buttions for the current question
+   /* func setQuestion() {
+        lblQuestion.text = currentQuestion!.question
+        answer0.setTitle(currentQuestion!.answers[0], for: .normal)
+        answer1.setTitle(currentQuestion!.answers[1], for: .normal)
+        answer2.setTitle(currentQuestion!.answers[2], for: .normal)
+        answer3.setTitle(currentQuestion!.answers[3], for: .normal)
+        lblProgress.text = "\(currentQuestionPos + 1) / \(questions.count)"
+    } */
     
     
     /*
